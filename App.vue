50f51b300aca8ee569004c54da2f14a66c3fd477
yqCherish <yq_cherish@qq.com>
2021-06-23 21:01:23 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <router-view class="router-view" />
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {},
+  mounted() {
+    //
+  }
+}
+</script>
+
+<style>
+.animation-style {
+  max-width: 460px;
+  margin: 0 auto;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
